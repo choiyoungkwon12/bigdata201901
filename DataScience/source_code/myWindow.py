ee2b7db2b01d370214c5235381312b41f138b949
choiyoungkwon12 <47075043+choiyoungkwon12@users.noreply.github.com>
2019-03-03 17:51:55 +0900

    Add files via upload

---
+++ DataScience/source_code/myWindow.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from tkinter import *
+
+
+# In[6]:
+
+
+w = Tk()
+w.title("나의 그래픽 프로그램")
+b = Button(w, text="나는 버튼이야")
+b.pack()
+b2 = Button(w, text="나도 버튼이야")
+b2.pack()
+
+w.mainloop()
+
+
+# In[ ]:
+
+
+
+
